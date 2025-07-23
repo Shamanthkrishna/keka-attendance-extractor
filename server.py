f2a5dd432fe58f57a4b316ddfb1a6786d680ff58
Shamanth Krishna <shamanth.krishna@tismotech.com>
2025-07-23 15:16:01 +0530

    Updated

--- server.py
+++ server.py
@@ -2,17 +2,20 @@ from flask import Flask, request, jsonify
 import threading
 from main import main, log_message, start_gui
 from flask_cors import CORS
+from waitress import serve
+
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/extract", methods=["POST"])
 def extract():
+    log_message("Received /extract request")
     token = request.json.get("token")
     if token:
         try:
             # Start GUI in background
-            threading.Thread(target=lambda: start_gui(disable_input=True), daemon=True).start()
+            # threading.Thread(target=lambda: start_gui(disable_input=True), daemon=True).start()
 
             # Wait until log_box is initialized
             from main import log_box_ready
@@ -21,6 +24,7 @@ def extract():
             log_message("Received token from extension")
             main(token)
 
+            log_message("Finished /extract request")
             return jsonify({"status": "success"}), 200
         except Exception as e:
             log_message(f"Exception during /extract: {e}")
@@ -32,4 +36,4 @@ def extract():
 
 # Start Flask in a separate thread if needed
 if __name__ == "__main__":
-    app.run(port=5000)
+    serve(app, port=5000)
